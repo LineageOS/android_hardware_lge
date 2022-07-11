fefdc3957ca7a836aea7c485c6587d57d0a92daa
LuK1337 <priv.luk@gmail.com>
2022-07-11 11:27:20 -0700

    amplifier: Add (and run) clang-format file
    Change-Id: Iaa76db2b802de191e5b494fc0ec91988efc13ff5

--- amplifier/amplifier.c
+++ amplifier/amplifier.c
@@ -27,7 +27,7 @@
 
 typedef struct lge_amplifier_device {
     amplifier_device_t amp_dev;
-    struct audio_device *adev;
+    struct audio_device* adev;
     struct audio_usecase* usecase_tx;
     struct pcm* pcm_out;
     bool hifi_dac_enabled;
@@ -52,7 +52,7 @@ static int lge_amplifier_is_speaker(uint32_t snd_device) {
 }
 
 int lge_amplifier_start_feedback(amplifier_device_t* device, uint32_t snd_device) {
-    lge_amplifier_device_t *lge_amplifier = (lge_amplifier_device_t*) device;
+    lge_amplifier_device_t* lge_amplifier = (lge_amplifier_device_t*)device;
     int pcm_dev_tx_id = 0, rc = 0;
     struct pcm_config pcm_config_lge_amplifier = {
             .channels = 2,
@@ -88,7 +88,8 @@ int lge_amplifier_start_feedback(amplifier_device_t* device, uint32_t snd_device
     enable_snd_device(lge_amplifier->adev, lge_amplifier->usecase_tx->in_snd_device);
     enable_audio_route(lge_amplifier->adev, lge_amplifier->usecase_tx);
 
-    pcm_dev_tx_id = platform_get_pcm_device_id(lge_amplifier->usecase_tx->id, lge_amplifier->usecase_tx->type);
+    pcm_dev_tx_id = platform_get_pcm_device_id(lge_amplifier->usecase_tx->id,
+                                               lge_amplifier->usecase_tx->type);
     ALOGD("pcm_dev_tx_id = %d", pcm_dev_tx_id);
     if (pcm_dev_tx_id < 0) {
         ALOGE("%d: Invalid pcm device for usecase (%d)", __LINE__, lge_amplifier->usecase_tx->id);
@@ -96,8 +97,8 @@ int lge_amplifier_start_feedback(amplifier_device_t* device, uint32_t snd_device
         goto error;
     }
 
-    lge_amplifier->pcm_out =
-            pcm_open(lge_amplifier->adev->snd_card, pcm_dev_tx_id, PCM_IN, &pcm_config_lge_amplifier);
+    lge_amplifier->pcm_out = pcm_open(lge_amplifier->adev->snd_card, pcm_dev_tx_id, PCM_IN,
+                                      &pcm_config_lge_amplifier);
     if (!(lge_amplifier->pcm_out || pcm_is_ready(lge_amplifier->pcm_out))) {
         ALOGE("%d: %s", __LINE__, pcm_get_error(lge_amplifier->pcm_out));
         rc = -EIO;
@@ -127,7 +128,7 @@ error:
 }
 
 void lge_amplifier_stop_feedback(amplifier_device_t* device, uint32_t snd_device) {
-    lge_amplifier_device_t *lge_amplifier = (lge_amplifier_device_t*) device;
+    lge_amplifier_device_t* lge_amplifier = (lge_amplifier_device_t*)device;
 
     if (!lge_amplifier) {
         ALOGE("%s: Invalid params", __func__);
@@ -143,7 +144,8 @@ void lge_amplifier_stop_feedback(amplifier_device_t* device, uint32_t snd_device
 
     disable_snd_device(lge_amplifier->adev, SND_DEVICE_IN_CAPTURE_VI_FEEDBACK);
 
-    lge_amplifier->usecase_tx = get_usecase_from_list(lge_amplifier->adev, USECASE_AUDIO_SPKR_CALIB_TX);
+    lge_amplifier->usecase_tx =
+            get_usecase_from_list(lge_amplifier->adev, USECASE_AUDIO_SPKR_CALIB_TX);
     if (lge_amplifier->usecase_tx) {
         list_remove(&lge_amplifier->usecase_tx->list);
         disable_audio_route(lge_amplifier->adev, lge_amplifier->usecase_tx);
@@ -153,7 +155,7 @@ void lge_amplifier_stop_feedback(amplifier_device_t* device, uint32_t snd_device
 }
 
 static int lge_amplifier_set_output_devices(struct amplifier_device* device, uint32_t devices) {
-    lge_amplifier_device_t *lge_amplifier = (lge_amplifier_device_t*) device;
+    lge_amplifier_device_t* lge_amplifier = (lge_amplifier_device_t*)device;
     bool want_to_enable_hifi_dac = property_get_bool("persist.vendor.audio.hifi.enabled", false);
 
     ALOGD("%s: enter", __func__);
@@ -169,42 +171,41 @@ static int lge_amplifier_set_output_devices(struct amplifier_device* device, uin
     }
 
     // If DAC is currently not enabled, but the prop is set to true, change paths
-    if(want_to_enable_hifi_dac) {
-        platform_set_snd_device_backend(SND_DEVICE_OUT_HEADPHONES, HIFI_DAC_BACKEND, 
-            HIFI_DAC_INTERFACE);
+    if (want_to_enable_hifi_dac) {
+        platform_set_snd_device_backend(SND_DEVICE_OUT_HEADPHONES, HIFI_DAC_BACKEND,
+                                        HIFI_DAC_INTERFACE);
         audio_route_apply_and_update_path(lge_amplifier->adev->audio_route, HIFI_DAC_MIXER_PATH);
         lge_amplifier->hifi_dac_enabled = true;
     }
     // In all other cases, go back to normal state
     else {
-        platform_set_snd_device_backend(SND_DEVICE_OUT_HEADPHONES, DEFAULT_BACKEND, 
-            DEFAULT_INTERFACE);
-        audio_route_apply_and_update_path(lge_amplifier->adev->audio_route, 
-            DEFAULT_MIXER_PATH);
+        platform_set_snd_device_backend(SND_DEVICE_OUT_HEADPHONES, DEFAULT_BACKEND,
+                                        DEFAULT_INTERFACE);
+        audio_route_apply_and_update_path(lge_amplifier->adev->audio_route, DEFAULT_MIXER_PATH);
         lge_amplifier->hifi_dac_enabled = false;
     }
     return 0;
 }
 
-static int lge_amplifier_set_parameters(struct amplifier_device *device, struct str_parms *parms) {
-    lge_amplifier_device_t *lge_amplifier = (lge_amplifier_device_t*) device;
+static int lge_amplifier_set_parameters(struct amplifier_device* device, struct str_parms* parms) {
+    lge_amplifier_device_t* lge_amplifier = (lge_amplifier_device_t*)device;
     int ret = 0, err, len;
-    char *kv_pairs = str_parms_to_str(parms);
-    char *value = NULL;
+    char* kv_pairs = str_parms_to_str(parms);
+    char* value = NULL;
 
-    if(lge_amplifier == NULL) {
+    if (lge_amplifier == NULL) {
         ret = -ENOMEM;
         ALOGE("%s: amplifier device is NULL", __func__);
         goto done;
     }
 
-    if(lge_amplifier->adev == NULL) {
+    if (lge_amplifier->adev == NULL) {
         ret = -ENOMEM;
         ALOGE("%s: adev is NULL", __func__);
         goto done;
     }
 
-    if(kv_pairs == NULL) {
+    if (kv_pairs == NULL) {
         ret = -ENOMEM;
         ALOGE("%s: key-value pair is NULL", __func__);
         goto done;
@@ -214,7 +215,7 @@ static int lge_amplifier_set_parameters(struct amplifier_device *device, struct 
 
     len = strlen(kv_pairs);
     value = (char*)calloc(len, sizeof(char));
-    if(value == NULL) {
+    if (value == NULL) {
         ret = -ENOMEM;
         ALOGE("%s: failed to allocate memory", __func__);
         goto done;
@@ -224,7 +225,7 @@ static int lge_amplifier_set_parameters(struct amplifier_device *device, struct 
     err = str_parms_get_str(parms, "hifi_dac", value, sizeof(value));
     if (err >= 0) {
         if (!strncmp(value, "on", 2)) {
-            if(lge_amplifier->hifi_dac_enabled) {
+            if (lge_amplifier->hifi_dac_enabled) {
                 ALOGD("%s: already enabled!", __func__);
                 ret = -EINVAL;
                 goto done;
@@ -245,8 +246,8 @@ static int lge_amplifier_set_parameters(struct amplifier_device *device, struct 
             goto done;
         }
 
-        struct audio_usecase *usecase;
-        struct listnode *node;
+        struct audio_usecase* usecase;
+        struct listnode* node;
         list_for_each(node, &lge_amplifier->adev->usecase_list) {
             usecase = node_to_item(node, struct audio_usecase, list);
             // If this was triggered during playback, switch immediately!
@@ -259,7 +260,7 @@ static int lge_amplifier_set_parameters(struct amplifier_device *device, struct 
                 /*
                     HACK: when selecting devices, and the device is the same, QCOM audio HAL does
                     not re-configure devices
-                    Unfortunately, we are unable to define our own device for the DAC, so the 
+                    Unfortunately, we are unable to define our own device for the DAC, so the
                     audio HAL will not re-configure, thus we cannot turn it on (if only we could
                     just clone the HAL...)
                     We can abuse the `force_device_switch()` function, that would do it for us
@@ -276,16 +277,14 @@ static int lge_amplifier_set_parameters(struct amplifier_device *device, struct 
     }
 done:
     ALOGV("%s: exit with code(%d)", __func__, ret);
-    if(kv_pairs != NULL)
-        free(kv_pairs);
-    if(value != NULL)
-        free(value);
+    if (kv_pairs != NULL) free(kv_pairs);
+    if (value != NULL) free(value);
     return ret;
 }
 
 static int lge_amplifier_set_feedback(amplifier_device_t* device, void* adev, uint32_t devices,
-                            bool enable) {
-    lge_amplifier_device_t *lge_amplifier = (lge_amplifier_device_t*) device;
+                                      bool enable) {
+    lge_amplifier_device_t* lge_amplifier = (lge_amplifier_device_t*)device;
     lge_amplifier->adev = (struct audio_device*)adev;
 
     if (enable) {
@@ -297,8 +296,8 @@ static int lge_amplifier_set_feedback(amplifier_device_t* device, void* adev, ui
 }
 
 // We're not really calibrating anything. We just need to save the audio_device.
-static int lge_amplifier_calibrate(struct amplifier_device *device, void *adev) {
-    lge_amplifier_device_t *lge_amplifier = (lge_amplifier_device_t*) device;
+static int lge_amplifier_calibrate(struct amplifier_device* device, void* adev) {
+    lge_amplifier_device_t* lge_amplifier = (lge_amplifier_device_t*)device;
 
     if (!adev) {
         ALOGE("%s: adev is NULL", __func__);
@@ -316,7 +315,7 @@ static int lge_amplifier_dev_close(hw_device_t* device) {
 }
 
 static int lge_amplifier_module_open(const hw_module_t* module, const char* name,
-                                hw_device_t** device) {
+                                     hw_device_t** device) {
     lge_amplifier_device_t* lge_amplifier;
 
     if (strcmp(name, AMPLIFIER_HARDWARE_INTERFACE)) {
