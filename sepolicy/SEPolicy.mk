6ea1a1c4f732d4c9e638239e620be53c24713d9f
Han Sol Jin <hansol@hansol.ca>
2023-12-21 14:33:34 -0800

    sepolicy: split vndr_legacy-um and legacy-um
    As more devices are starting to use the code in this repository, there
    are conflicts with SELinux policies as labels have switched to
    vendor_sysfs in some places. This causes conflicts in new and old devices.
    
    Allow platform-specific policies to inherit certain folders and rules.
    
    Change-Id: I9cb82056fc25a067adea511e6dec0b0e4d6b9bf9

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr-legacy-um)
+	SEPOLICY_PLATFORM := vndr
+else
+	SEPOLICY_PLATFORM := legacy-um
+endif
+
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/lge/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/lge/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/lge/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += \
+	hardware/lge/sepolicy/vendor \
+	hardware/lge/sepolicy/vendor/$(SEPOLICY_PLATFORM)
