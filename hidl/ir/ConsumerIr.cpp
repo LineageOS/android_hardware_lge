c6547f8f0d8b79525bdf73582e82582fb03673d1
Han Sol Jin <hansol@hansol.ca>
2023-12-31 10:07:56 -0800

    ir: add transmitIr() return value enum
    Based on observing stock IR HAL expectations.
    
    This also fixes a logic issue when returning from ConsumerIr::transmit.
    
    Change-Id: Ia4802ce8f3aea60f1728ac75cb432164b44b832a

--- hidl/ir/ConsumerIr.cpp
+++ hidl/ir/ConsumerIr.cpp
@@ -41,14 +41,33 @@ static hidl_vec<ConsumerIrFreqRange> rangeVec{
     {.min = 25000, .max = 125000},
 };
 
+enum LG_TRANSMIT_IR_RETURN_CODES {
+    IR_SUCCESS,
+    IR_FAIL,
+    IR_INVALID_PORT,
+    IR_INVALID_BAUDRATE,
+    IR_ERROR_OPEN_PORT,
+    IR_ERROR_WRITE,
+    IR_ERROR_READ,
+    IR_INVALID_DATA_SIZE,
+    IR_INVALID_DATA,
+    IR_MAX_DURATIONS_EXCEEDED
+};
+
 ConsumerIr::ConsumerIr() {}
 
 Return<bool> ConsumerIr::transmit(int32_t carrierFreq, const hidl_vec<int32_t>& pattern) {
     size_t entries = pattern.size();
+    int rc;
 
     // call into libcir_driver
     ALOGD("transmitting pattern at %d Hz", carrierFreq);
-    return transmitIr(IR_DEVICE, LG_IR_BAUD_RATE, carrierFreq, const_cast<int32_t*>(pattern.data()), sizeof(int32_t) * entries);
+    rc = transmitIr(IR_DEVICE, LG_IR_BAUD_RATE, carrierFreq, const_cast<int32_t*>(pattern.data()), sizeof(int32_t) * entries);
+    if (rc != IR_SUCCESS) {
+        ALOGE("transmitIr() failed, error %d\n", rc);
+        return false;
+    }
+    return true;
 }
 
 Return<void> ConsumerIr::getCarrierFreqs(getCarrierFreqs_cb _hidl_cb) {
