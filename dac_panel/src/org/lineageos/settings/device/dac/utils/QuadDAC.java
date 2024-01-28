632c5f96ec1e3efbf7bc0e0c0febf29685ae1cb9
Han Sol Jin <hansol@hansol.ca>
2024-01-28 09:28:26 -0800

    dac_panel: abstract IDacControl to utils/QuadDAC.java
    Do not access IDacControl from anywhere else. Use QuadDAC class to
    interface with the service.
    
    Change-Id: I49a9026b09a49f746920b18304cf5f704e0a75cf

--- dac_panel/src/org/lineageos/settings/device/dac/utils/QuadDAC.java
+++ dac_panel/src/org/lineageos/settings/device/dac/utils/QuadDAC.java
@@ -4,7 +4,10 @@ import android.media.AudioSystem;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 
+import java.util.ArrayList;
+
 import vendor.lge.hardware.audio.dac.control.V2_0.Feature;
+import vendor.lge.hardware.audio.dac.control.V2_0.FeatureStates;
 import vendor.lge.hardware.audio.dac.control.V2_0.IDacControl;
 
 public class QuadDAC {
@@ -13,91 +16,112 @@ public class QuadDAC {
 
     private QuadDAC() {}
 
-    public static void enable(IDacControl dac)
+    private static IDacControl dac;
+
+    private static ArrayList<Integer> dac_features;
+
+    public static boolean dac_service_available = false;
+
+    public static void initialize() throws RemoteException {
+        dac = IDacControl.getService(true);
+        dac_features = dac.getSupportedFeatures();
+        dac_service_available = true;
+    }
+
+    public static void enable()
     {
         try {
-            int digital_filter = getDigitalFilter(dac);
-            int sound_preset = getSoundPreset(dac);
-            int left_balance = getLeftBalance(dac);
-            int right_balance = getRightBalance(dac);
-            int mode = getDACMode(dac);
-            int avc_vol = getAVCVolume(dac);
+            int digital_filter = getDigitalFilter();
+            int sound_preset = getSoundPreset();
+            int left_balance = getLeftBalance();
+            int right_balance = getRightBalance();
+            int mode = getDACMode();
+            int avc_vol = getAVCVolume();
             dac.setHifiDacState(true);
-            setDACMode(dac, mode);
-            setLeftBalance(dac, left_balance);
-            setRightBalance(dac, right_balance);
-            setDigitalFilter(dac, digital_filter);
-            setSoundPreset(dac, sound_preset);
-            setAVCVolume(dac, avc_vol);
+            setDACMode(mode);
+            setLeftBalance(left_balance);
+            setRightBalance(right_balance);
+            setDigitalFilter(digital_filter);
+            setSoundPreset(sound_preset);
+            setAVCVolume(avc_vol);
         } catch(Exception e) {}
     }
 
-    public static void disable(IDacControl dac) throws RemoteException
+    public static void disable() throws RemoteException
     {
         dac.setHifiDacState(false);
     }
 
-    public static void setDACMode(IDacControl dac, int mode) throws RemoteException
+    public static ArrayList<Integer> getSupportedFeatures() {
+        return dac_features;
+    }
+
+    public static FeatureStates getSupportedFeatureValues(int feature) throws RemoteException
+    {
+        return dac.getSupportedFeatureValues(feature);
+    }
+
+    public static void setDACMode(int mode) throws RemoteException
     {
         dac.setFeatureValue(Feature.HifiMode, mode);
     }
 
-    public static int getDACMode(IDacControl dac) throws RemoteException
+    public static int getDACMode() throws RemoteException
     {
         return dac.getFeatureValue(Feature.HifiMode);
     }
 
-    public static void setAVCVolume(IDacControl dac, int avc_volume) throws RemoteException
+    public static void setAVCVolume(int avc_volume) throws RemoteException
     {
         dac.setFeatureValue(Feature.AVCVolume, avc_volume);
     }
 
-    public static int getAVCVolume(IDacControl dac) throws RemoteException
+    public static int getAVCVolume() throws RemoteException
     {
         return dac.getFeatureValue(Feature.AVCVolume);
     }
 
-    public static void setDigitalFilter(IDacControl dac, int filter) throws RemoteException
+    public static void setDigitalFilter(int filter) throws RemoteException
     {
         dac.setFeatureValue(Feature.DigitalFilter, filter);
     }
 
-    public static int getDigitalFilter(IDacControl dac) throws RemoteException
+    public static int getDigitalFilter() throws RemoteException
     {
         return dac.getFeatureValue(Feature.DigitalFilter);
     }
 
-    public static void setSoundPreset(IDacControl dac, int preset) throws RemoteException
+    public static void setSoundPreset(int preset) throws RemoteException
     {
         dac.setFeatureValue(Feature.SoundPreset, preset);
     }
 
-    public static int getSoundPreset(IDacControl dac) throws RemoteException
+    public static int getSoundPreset() throws RemoteException
     {
         return dac.getFeatureValue(Feature.SoundPreset);
     }
 
-    public static void setLeftBalance(IDacControl dac, int balance) throws RemoteException
+    public static void setLeftBalance(int balance) throws RemoteException
     {
         dac.setFeatureValue(Feature.BalanceLeft, balance);
     }
 
-    public static int getLeftBalance(IDacControl dac) throws RemoteException
+    public static int getLeftBalance() throws RemoteException
     {
         return dac.getFeatureValue(Feature.BalanceLeft);
     }
 
-    public static void setRightBalance(IDacControl dac, int balance) throws RemoteException
+    public static void setRightBalance(int balance) throws RemoteException
     {
         dac.setFeatureValue(Feature.BalanceRight, balance);
     }
 
-    public static int getRightBalance(IDacControl dac) throws RemoteException
+    public static int getRightBalance() throws RemoteException
     {
         return dac.getFeatureValue(Feature.BalanceRight);
     }
 
-    public static boolean isEnabled(IDacControl dac) throws RemoteException
+    public static boolean isEnabled() throws RemoteException
     {
         return dac.getHifiDacState();
     }
