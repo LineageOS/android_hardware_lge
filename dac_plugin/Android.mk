9a22ceff29c03f22a9c73f5404c5b11d911c99c1
Han Sol Jin <hansol@hansol.ca>
2024-04-21 12:25:32 -0700

    hardware/lge: add a horrible hack to get DAC working with OSS audio HAL
    This is an awful implementation to enable the Hi-Fi Quad DAC feature.
    
    Due to cloning our own audio HAL causing several Soong issues, instead
    opt to hijack the audio_amplifier HAL.
    
    This has the limitation that we cannot introduce our own snd_device,
    backend, or interface directly to the platform list in the HAL; to
    address this, we can force SND_DEVICE_OUT_HEADPHONES to switch to LG's
    custom backend and use those in place of the default backend. When
    disabling, the original backend should be brought back.
    
    To use this, add TARGET_SPECIFIC_HEADER_PATH, copy dac_plugin.h to the
    device repository's header path, and rename the mixer paths, backends
    and interfaces as needed.
    
    Change-Id: I4d61a288746c2eb34849831d58633c298e04622e

---
+++ dac_plugin/Android.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2024 The LineageOS Project
+#
+# * SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := audio_amplifier.hifi_dac
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := dac_plugin.c
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_C_INCLUDES += \
+    $(call include-path-for, audio-route) \
+    $(call include-path-for, audio-utils) \
+    $(call project-path-for, qcom-audio)/hal \
+    $(call project-path-for, qcom-audio)/hal/audio_extn \
+    $(call project-path-for, qcom-audio)/hal/msm8974 \
+    external/tinycompress/include \
+    $(LOCAL_PATH)/include
+
+LOCAL_HEADER_LIBRARIES += \
+    generated_kernel_headers \
+    libhardware_headers
+
+LOCAL_SHARED_LIBRARIES += \
+    audio.primary.$(TARGET_BOARD_PLATFORM) \
+    libaudioroute \
+    libcutils \
+    liblog \
+    libtinyalsa
+
+LOCAL_CFLAGS += \
+    -Wno-unused-parameter
+
+ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+LOCAL_CFLAGS += \
+    -DTARGET_LEGACY_UM
+endif
+
+include $(BUILD_SHARED_LIBRARY)
