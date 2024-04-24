10513dc9713a8eb5f1b530d5e14162ec1b289769
Han Sol Jin <hansol@hansol.ca>
2024-04-24 11:39:38 -0700

    radio: setup LgeRadio
    Change-Id: Id87c0d692e04d0b11230170f4d5cc2db5a9a3e52

--- hidl/radio/service.cpp
+++ hidl/radio/service.cpp
@@ -7,6 +7,7 @@
 #define LOG_TAG "android.hardware.radio@1.4-service.lge"
 
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <hidl/HidlTransportSupport.h>
 
 #include "Radio.h"
@@ -17,6 +18,7 @@
 using namespace android::hardware::radio;
 using namespace android::hardware::hidl_utils;
 
+using android::base::GetIntProperty;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
@@ -26,12 +28,11 @@ using android::OK;
 using android::sp;
 using android::status_t;
 
-#define MAX_SLOT_ID 4
-
 int main() {
     // Note: Starts from slot 1
     std::map<int, sp<V1_4::IRadio>> slotIdToRadio;
 
+    int MAX_SLOT_ID = GetIntProperty("ro.boot.vendor.lge.sim_num", 1);
     for (int slotId = 1; slotId <= MAX_SLOT_ID; slotId++) {
         sp<V1_0::IRadio> realRadio = V1_0::IRadio::getService("slot" + std::to_string(slotId));
         if (realRadio == nullptr) {
@@ -45,11 +46,11 @@ int main() {
             break;
         }
 
-        slotIdToRadio[slotId] = new Radio(realRadio);
+        slotIdToRadio[slotId] = new Radio(realRadio, slotId);
         linkDeathToDeath(realRadio);
     }
 
-    configureRpcThreadpool(1, true);
+    configureRpcThreadpool(MAX_SLOT_ID * 2 + 2, true);
 
     for (auto const& [slotId, radio] : slotIdToRadio) {
         status_t status = radio->registerAsService("slot" + std::to_string(slotId));
