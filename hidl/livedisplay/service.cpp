480b438e5f2a0cb0bedf6079ac7d9cf9da917cd4
Han Sol Jin <hansol@hansol.ca>
2024-05-27 21:17:18 -0700

    livedisplay: migrate to using fragments from hardware/lineage/livedisplay
    Change-Id: I7ef374b75e30d4ac9fd5f38d7944a2c59999d0c6

--- hidl/livedisplay/service.cpp
+++ hidl/livedisplay/service.cpp
@@ -1,10 +1,8 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2021 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
-#include <dlfcn.h>
 
 #define LOG_TAG "vendor.lineage.livedisplay@2.0-service.lge"
 
@@ -12,14 +10,11 @@
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
 
-#include "ColorEnhancement.h"
-#include "DisplayModes.h"
-#include "PictureAdjustment.h"
-#include "SunlightEnhancement.h"
+#include <livedisplay/lge/ColorEnhancement.h>
+#include <livedisplay/lge/DisplayModes.h>
+#include <livedisplay/lge/SunlightEnhancement.h>
 
-constexpr const char* SDM_DISP_LIBS[]{
-        "libsdm-disp-vndapis.so",
-};
+#include <livedisplay/sdm/PictureAdjustment.h>
 
 using android::OK;
 using android::sp;
@@ -33,116 +28,66 @@ using ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
 using ::vendor::lineage::livedisplay::V2_0::implementation::ColorEnhancement;
 using ::vendor::lineage::livedisplay::V2_0::implementation::DisplayModes;
-using ::vendor::lineage::livedisplay::V2_0::implementation::PictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
 
+using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
+using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
+
 int main() {
-    // Vendor backend
-    void* libHandle = nullptr;
-    const char* libName = nullptr;
-    int32_t (*disp_api_init)(uint64_t*, uint32_t) = nullptr;
-    int32_t (*disp_api_deinit)(uint64_t, uint32_t) = nullptr;
-    uint64_t cookie = 0;
-
-    // HIDL frontend
-    sp<ColorEnhancement> ce;
-    sp<DisplayModes> dm;
-    sp<PictureAdjustment> pa;
-    sp<SunlightEnhancement> se;
-
     status_t status = OK;
 
     android::ProcessState::initWithDriver("/dev/vndbinder");
 
     LOG(INFO) << "LiveDisplay HAL service is starting.";
 
+    std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
 
-    ce = new ColorEnhancement();
+    // HIDL frontend
+    sp<ColorEnhancement> ce = new ColorEnhancement();;
+    sp<DisplayModes> dm = new DisplayModes();
+    sp<SunlightEnhancement> se = new SunlightEnhancement();
+    sp<PictureAdjustment> pa = new PictureAdjustment(controller);
+
     if (ce == nullptr) {
         LOG(ERROR)
                 << "Can not create an instance of LiveDisplay HAL ColorEnhancement Iface, exiting.";
         goto shutdown;
     }
 
-    dm = new DisplayModes();
     if (dm == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL DisplayModes Iface,"
                    << " exiting.";
         goto shutdown;
     }
 
-    se = new SunlightEnhancement();
     if (se == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL SunlightEnhancement Iface, exiting.";
         goto shutdown;
     }
 
-    for (auto&& lib : SDM_DISP_LIBS) {
-        libHandle = dlopen(lib, RTLD_NOW);
-        libName = lib;
-        if (libHandle != nullptr) {
-            LOG(INFO) << "Loaded: " << libName;
-            break;
-        }
-        LOG(ERROR) << "Can not load " << libName << " (" << dlerror() << ")";
-    }
-
-    if (libHandle == nullptr) {
-        LOG(ERROR) << "Failed to load SDM display lib, exiting.";
-        goto shutdown;
-    }
-
-    disp_api_init =
-            reinterpret_cast<int32_t (*)(uint64_t*, uint32_t)>(dlsym(libHandle, "disp_api_init"));
-    if (disp_api_init == nullptr) {
-        LOG(ERROR) << "Can not get disp_api_init from " << libName << " (" << dlerror() << ")";
-        goto shutdown;
-    }
-
-    disp_api_deinit =
-            reinterpret_cast<int32_t (*)(uint64_t, uint32_t)>(dlsym(libHandle, "disp_api_deinit"));
-    if (disp_api_deinit == nullptr) {
-        LOG(ERROR) << "Can not get disp_api_deinit from " << libName << " (" << dlerror() << ")";
-        goto shutdown;
-    }
-
-    status = disp_api_init(&cookie, 0);
-    if (status != OK) {
-        LOG(ERROR) << "Can not initialize " << libName << " (" << status << ")";
-        goto shutdown;
-    }
-
-    pa = new PictureAdjustment(libHandle, cookie);
-    if (pa == nullptr) {
-        LOG(ERROR) << "Can not create an instance of LiveDisplay HAL PictureAdjustment Iface,"
-                   << " exiting.";
-        goto shutdown;
-    }
-
-    if (!pa->isSupported()) {
-        // Backend isn't ready yet, so restart and try again
-        goto shutdown;
-    }
-
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    if (ce->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register ColorEnhancement HAL service.";
-    }
-
-    if (dm->registerAsService() != android::OK) {
+    status = dm->registerAsService();
+    if (status != OK) {
         LOG(ERROR) << "Cannot register DisplayModes HAL service.";
         goto shutdown;
     }
 
-    if (se->registerAsService() != OK) {
+    status = se->registerAsService();
+    if (status != OK) {
         LOG(ERROR) << "Could not register service for LiveDisplay HAL SunlightEnhancement Iface ("
                    << status << ")";
     }
 
-    if (pa->registerAsService() != android::OK) {
+    status = pa->registerAsService();
+    if (status != OK) {
         LOG(ERROR) << "Cannot register PictureAdjustment HAL service.";
         goto shutdown;
+    }
+
+    status = ce->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register ColorEnhancement HAL service.";
     }
 
     LOG(INFO) << "LiveDisplay HAL service is ready.";
