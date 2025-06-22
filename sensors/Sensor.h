bulla0x/android_hardware_xiaomi

sensors: Implement UDFPS sensor
Change-Id: I8b65e3196a7d6f158f4ff589b9755cf7c7e51c92

sensors/Sensor.h
@@ -133,6 +133,18 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
               static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 1)) {}
 };
 
+const std::string kTsUdfpsPressedPath = kTsPath + "udfps_pressed";
+const std::string kTsUdfpsEnabledPath = kTsPath + "udfps_enabled";
+
+class UdfpsSensor : public SysfsPollingOneShotSensor {
+  public:
+    UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+              sensorHandle, callback, kTsUdfpsPressedPath, kTsUdfpsEnabledPath,
+              "UDFPS Sensor", "org.lineageos.sensor.udfps",
+              static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 2)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1

LineageOS/android_hardware_lge

sensors/Sensor.h
@@ -120,6 +120,20 @@ class SysfsPollingOneShotSensor : public OneShotSensor {
     int mPollFd;
 };
 
+const std::string kTsPath = "/sys/devices/virtual/input/lge_touch/";
+
+const std::string kTsUdfpsPressedPath = kTsPath + "udfps_pressed";
+const std::string kTsUdfpsEnabledPath = kTsPath + "longpress";
+
+class UdfpsSensor : public SysfsPollingOneShotSensor {
+  public:
+    UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+              sensorHandle, callback, kTsUdfpsPressedPath, kTsUdfpsEnabledPath,
+              "UDFPS Sensor", "org.lineageos.sensor.udfps",
+              static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 2)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1