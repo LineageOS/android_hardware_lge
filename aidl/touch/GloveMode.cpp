c129bc9c2597c49cb208fe00f278edae59223cb9
Han Sol Jin <hansol@hansol.ca>
2025-06-22 14:01:39 -0700

    touch: wire up GloveMode
    Co-authored-by: SGCMarkus <markusornik@gmail.com>
    Change-Id: Id0675d3d45996fca8750bc3acaf4cd9bdd30ee3b

--- aidl/touch/GloveMode.cpp
+++ aidl/touch/GloveMode.cpp
@@ -3,19 +3,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define LOG_TAG "GloveModeService"
+
 #include "GloveMode.h"
+
+#include <fstream>
 
 namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
 
-ndk::ScopedAStatus GloveMode::getEnabled(bool* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+const std::string kFilmStatusPath = "/sys/devices/virtual/input/lge_touch/film_status";
+
+ndk::ScopedAStatus GloveMode::getEnabled(bool* _aidl_return) {
+    std::ifstream file(kFilmStatusPath);
+    int enabled;
+    file >> enabled;
+
+    if(enabled == 1)
+    *_aidl_return = (enabled == 1);
+
+    return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus GloveMode::setEnabled(bool /*enabled*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+ndk::ScopedAStatus GloveMode::setEnabled(bool enabled) {
+    std::ofstream file(kFilmStatusPath);
+    file << (enabled ? "1" : "0");
+    return !file.fail() ? ndk::ScopedAStatus::ok() : ndk::ScopedAStatus::fromExceptionCode(EX_TRANSACTION_FAILED);
 }
 
 } // namespace touch
