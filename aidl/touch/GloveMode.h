c7b71126ef9dfbe42533e2f955a2c17405ed203b
Han Sol Jin <hansol@hansol.ca>
2025-06-22 13:56:09 -0700

    aidl: add Touch AIDL skeleton
    Change-Id: I5a7a9b1aad0ec604ef128090f4df8d43186265e3

---
+++ aidl/touch/GloveMode.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/touch/BnGloveMode.h>
+
+
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace touch {
+
+class GloveMode : public BnGloveMode {
+public:
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;
+};
+
+} // namespace touch
+} // namespace lineage
+} // namespace vendor
+} // namespace aidl
---
+++ aidl/touch/TouchscreenGesture.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/touch/BnTouchscreenGesture.h>
+
+using ::aidl::vendor::lineage::touch::Gesture;
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace touch {
+
+class TouchscreenGesture : public BnTouchscreenGesture {
+public:
+    ndk::ScopedAStatus getSupportedGestures(std::vector<Gesture>* _aidl_return) override;
+    ndk::ScopedAStatus setGestureEnabled(const Gesture& gesture, bool enabled) override;
+};
+
+} // namespace touch
+} // namespace lineage
+} // namespace vendor
+} // namespace aidl
