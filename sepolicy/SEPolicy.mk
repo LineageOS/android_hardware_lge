07239642649c68d169b1e53ae983e74127d9967c
Han Sol Jin <hansol@hansol.ca>
2025-06-22 12:38:11 -0700

    sepolicy: add all remaining policies
    Merge LGE MSM8998, SDM845, SM8150 policies into a common location.
    
    Change-Id: Iba6b8732d5b6bea17338ce169ca8e188b692f799

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -13,6 +13,10 @@ else
 	SEPOLICY_PLATFORM := vndr
 endif
 
+ifeq ($(BOARD_LGE_USES_PERSDATA_AS_METADATA),true)
+	BOARD_SEPOLICY_M4DEFS += persdata_abt_block_device=metadata_block_device
+endif
+
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/lge/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/lge/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += \
