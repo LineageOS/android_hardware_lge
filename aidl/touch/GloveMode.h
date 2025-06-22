86287d4b7fa09e3a398020d011388d256e5bb787
Han Sol Jin <hansol@hansol.ca>
2025-06-22 13:56:09 -0700

    aidl: add Touch AIDL skeleton
    Change-Id: I5a7a9b1aad0ec604ef128090f4df8d43186265e3

---
+++ aidl/touch/GloveMode.h
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/touch/BnGloveMode.h>
+
+
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace touch {
+
+class GloveMode : public BnGloveMode {
+public:
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;
+};
+
+} // namespace touch
+} // namespace lineage
+} // namespace vendor
+} // namespace aidl
