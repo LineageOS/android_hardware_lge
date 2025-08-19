2ff8fde004358ce14d6453f6169b0523d4de5eea
Tuan Anh <tuan73176@gmail.com>
2025-08-19 04:16:53 +0200

    aidl: Migrate to AIDL LiveDisplay HAL
    Change-Id: I239acfd2e38cee00e8313b5bc0bef1f8aeca2a19

--- aidl/livedisplay/ColorEnhancement.cpp
+++ aidl/livedisplay/ColorEnhancement.cpp
@@ -1,14 +1,12 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
- *
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <livedisplay/lge/ColorEnhancement.h>
-
-#include <android-base/logging.h>
 #include <android-base/file.h>
+#include <android-base/logging.h>
 #include <android-base/strings.h>
+#include <livedisplay/lge/ColorEnhancement.h>
 
 #include <fstream>
 
@@ -16,11 +14,11 @@ using android::base::ReadFileToString;
 using android::base::Trim;
 using android::base::WriteStringToFile;
 
+namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
-namespace V2_0 {
-namespace implementation {
+namespace lge {
 
 static constexpr const char* kModePath = "/sys/devices/virtual/panel/img_tune/hdr_mode";
 static constexpr const char* kDefaultPath = "/data/misc/display/default_hdr_mode";
@@ -29,7 +27,8 @@ ColorEnhancement::ColorEnhancement() {
     std::ifstream defaultFile(kDefaultPath);
 
     defaultFile >> mDefaultColorEnhancement;
-    LOG(DEBUG) << "Default file read result " << mDefaultColorEnhancement << " fail " << defaultFile.fail();
+    LOG(DEBUG) << "Default file read result " << mDefaultColorEnhancement << " fail "
+               << defaultFile.fail();
     if (defaultFile.fail()) {
         return;
     }
@@ -37,8 +36,13 @@ ColorEnhancement::ColorEnhancement() {
     setEnabled(mDefaultColorEnhancement);
 }
 
+bool ColorEnhancement::isSupported() {
+    std::ofstream modeFile(kModePath);
+    return modeFile.good();
+}
+
 // Methods from ::vendor::lineage::livedisplay::V2_0::IColorEnhancement follow.
-Return<bool> ColorEnhancement::isEnabled() {
+ndk::ScopedAStatus ColorEnhancement::getEnabled(bool* _aidl_return) {
     std::string tmp;
     int32_t contents = 0;
 
@@ -46,17 +50,20 @@ Return<bool> ColorEnhancement::isEnabled() {
         contents = std::stoi(Trim(tmp));
     }
 
-    return contents > 0;
+    *_aidl_return = contents > 0;
+    return ndk::ScopedAStatus::ok();
 }
 
-Return<bool> ColorEnhancement::setEnabled(bool enabled) {
-    WriteStringToFile(enabled ? "1" : "0", kModePath, true);
+ndk::ScopedAStatus ColorEnhancement::setEnabled(bool enabled) {
+    if (!WriteStringToFile(enabled ? "1" : "0", kModePath, true)) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
 
-    return WriteStringToFile(enabled ? "1" : "0", kModePath, true);
+    return ndk::ScopedAStatus::ok();
 }
 
-}  // namespace implementation
-}  // namespace V2_0
+}  // namespace lge
 }  // namespace livedisplay
 }  // namespace lineage
 }  // namespace vendor
+}  // namespace aidl
---
+++ aidl/livedisplay/DisplayModes.cpp
@@ -0,0 +1,119 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/logging.h>
+#include <livedisplay/lge/DisplayModes.h>
+
+#include <fstream>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace lge {
+
+static constexpr const char* kModePath = "/sys/devices/virtual/panel/img_tune/screen_mode";
+static constexpr const char* kDefaultPath = "/data/misc/display/default_screen_mode";
+
+const std::map<int32_t, std::string> DisplayModes::kModeMap = {
+    // clang-format off
+    {0, "Auto"},
+    {1, "Cinema"},
+    {2, "Photos"},
+    {3, "Web"},
+    {4, "Sports"},
+    {5, "Game"},
+    // clang-format on
+};
+
+DisplayModes::DisplayModes() : mDefaultModeId(0) {
+    std::ifstream defaultFile(kDefaultPath);
+    int value;
+
+    defaultFile >> value;
+    LOG(DEBUG) << "Default file read result " << value << " fail " << defaultFile.fail();
+    if (defaultFile.fail()) {
+        return;
+    }
+
+    for (const auto& entry : kModeMap) {
+        if (value == entry.first) {
+            mDefaultModeId = entry.first;
+            break;
+        }
+    }
+
+    setDisplayMode(mDefaultModeId, false);
+}
+
+bool DisplayModes::isSupported() {
+    std::ofstream modeFile(kModePath);
+    return modeFile.good();
+}
+
+// Methods from ::vendor::lineage::livedisplay::V2_0::IDisplayModes follow.
+ndk::ScopedAStatus DisplayModes::getDisplayModes(std::vector<DisplayMode>* _aidl_return) {
+    int value;
+    std::vector<DisplayMode> modes;
+    value = kModeMap.size();
+
+    for (const auto& entry : kModeMap) {
+        if (entry.first < value) modes.push_back({entry.first, entry.second});
+    }
+
+    *_aidl_return = modes;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus DisplayModes::getCurrentDisplayMode(DisplayMode* _aidl_return) {
+    int32_t currentModeId = mDefaultModeId;
+    std::ifstream modeFile(kModePath);
+    int value;
+    modeFile >> value;
+    if (!modeFile.fail()) {
+        for (const auto& entry : kModeMap) {
+            if (value == entry.first) {
+                currentModeId = entry.first;
+                break;
+            }
+        }
+    }
+
+    *_aidl_return = DisplayMode{currentModeId, kModeMap.at(currentModeId)};
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus DisplayModes::getDefaultDisplayMode(DisplayMode* _aidl_return) {
+    *_aidl_return = DisplayMode{mDefaultModeId, kModeMap.at(mDefaultModeId)};
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus DisplayModes::setDisplayMode(int32_t modeID, bool makeDefault) {
+    const auto iter = kModeMap.find(modeID);
+    if (iter == kModeMap.end()) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+    std::ofstream modeFile(kModePath);
+    modeFile << iter->first;
+    if (modeFile.fail()) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    if (makeDefault) {
+        std::ofstream defaultFile(kDefaultPath);
+        defaultFile << iter->first;
+        if (defaultFile.fail()) {
+            return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+        }
+        mDefaultModeId = iter->first;
+    }
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace lge
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
