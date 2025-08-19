9f1cb280974f80e1f95222fd8e78d9ad60e0c885
Giovanni Ricca <giovanniricca@proton.me>
2025-08-19 04:16:53 +0200

    aidl: Initial LiveDisplay AIDL implementation
    Based on sm7250, sm8250 & sm8450 in-tree HIDL implementations.
    
    Change-Id: Ic044ef4261b2dfac2904609a6eb0ca6635474bc5
    
    aidl: Initial LiveDisplay AIDL implementation part 2
    
    Change-Id: Ic5ca0cca59592315f9fd717ebaa6019c0edb06d2
    
    LGE: Migrate aidl part 3
    
    Change-Id: I8ec55947436b7c462afb19e82be10f3a18c7fe25
    
    aidl: Initial LiveDisplay AIDL implementation part4
    
    Change-Id: I7405d28299d58f32a8ba0ca3c33c9bd860275603
    
    aidl: Initial LiveDisplay AIDL implementation 5/5
    
    Change-Id: Ibf6eb7f484d30e2a16b27caafda42b0e4ce6115d
    
    aidl:Livedisplay corrected SE Function
    
    Change-Id: I0437732cb3811742f06dea9f5c3147fe83e4f52d
    
    aidl: Initial LiveDisplay AIDL implementation
    
    Change-Id: I7be43341bc932ae93d6906ccfe063b959f273335

---
+++ aidl/livedisplay/service.cpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.livedisplay-service-lge"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <binder/ProcessState.h>
+#include <livedisplay/lge/SunlightEnhancement.h>
+
+using ::aidl::vendor::lineage::livedisplay::SunlightEnhancement;
+
+int main() {
+    android::ProcessState::self()->setThreadPoolMaxThreadCount(1);
+    android::ProcessState::self()->startThreadPool();
+
+    LOG(INFO) << "LiveDisplay HAL service is starting.";
+
+    std::shared_ptr<SunlightEnhancement> se =
+            ENABLE_SE ? ndk::SharedRefBase::make<SunlightEnhancement>() : nullptr;
+
+    if (se) {
+        std::string instance = std::string() + SunlightEnhancement::descriptor + "/default";
+        binder_status_t status = AServiceManager_addService(se->asBinder().get(), instance.c_str());
+        CHECK_EQ(status, STATUS_OK);
+    }
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}
