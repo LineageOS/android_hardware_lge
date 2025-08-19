353dfff18839096b5be6ae6f724b92f4fb5de8d0
Tuan Anh <tuan73176@gmail.com>
2025-08-19 04:16:53 +0200

    meme
    Change-Id: I239acfd2e38cee00e8313b5bc0bef1f8aeca2a19

---
+++ aidl/livedisplay/include/livedisplay/lge/SunlightEnhancement.h
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/livedisplay/BnSunlightEnhancement.h>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace implementation {
+
+class SunlightEnhancement : public BnSunlightEnhancement {
+  public:
+    // Methods from ::aidl::vendor::lineage::livedisplay::BnSunlightEnhancement follow.
+    ndk::ScopedAStatus isEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;};
+};
+
+}  // namespace implementation
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
