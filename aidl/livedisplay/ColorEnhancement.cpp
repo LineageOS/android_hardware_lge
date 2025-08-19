39ae7b854739cd1d93f4e0560814eb6a71dc8caa
Giovanni Ricca <giovanniricca@proton.me>
2025-08-19 04:16:53 +0200

    aidl: Migrate to LiveDisplay AIDL
    Loosely based on hardware/samsung.
    
    Change-Id: I239acfd2e38cee00e8313b5bc0bef1f8aeca2a19

--- aidl/livedisplay/ColorEnhancement.cpp
+++ aidl/livedisplay/ColorEnhancement.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
- *
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -9,54 +8,56 @@
 #include <android-base/logging.h>
 #include <android-base/file.h>
 #include <android-base/strings.h>
-
+ 
 #include <fstream>
-
+ 
 using android::base::ReadFileToString;
 using android::base::Trim;
 using android::base::WriteStringToFile;
 
+namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
-namespace V2_0 {
 namespace implementation {
-
+ 
 static constexpr const char* kModePath = "/sys/devices/virtual/panel/img_tune/hdr_mode";
 static constexpr const char* kDefaultPath = "/data/misc/display/default_hdr_mode";
-
+ 
 ColorEnhancement::ColorEnhancement() {
     std::ifstream defaultFile(kDefaultPath);
-
+ 
     defaultFile >> mDefaultColorEnhancement;
     LOG(DEBUG) << "Default file read result " << mDefaultColorEnhancement << " fail " << defaultFile.fail();
     if (defaultFile.fail()) {
         return;
     }
-
+ 
     setEnabled(mDefaultColorEnhancement);
 }
-
+ 
 // Methods from ::vendor::lineage::livedisplay::V2_0::IColorEnhancement follow.
-Return<bool> ColorEnhancement::isEnabled() {
+ndk::ScopedAStatus ColorEnhancement::isEnabled() {
     std::string tmp;
     int32_t contents = 0;
-
+ 
     if (ReadFileToString(kModePath, &tmp)) {
         contents = std::stoi(Trim(tmp));
     }
 
-    return contents > 0;
+    *_aidl_return = contents > 0;
+    return ndk::ScopedAStatus::ok();
 }
-
-Return<bool> ColorEnhancement::setEnabled(bool enabled) {
+ 
+ndk::ScopedAStatus ColorEnhancement::setEnabled(bool enabled) {
     WriteStringToFile(enabled ? "1" : "0", kModePath, true);
 
-    return WriteStringToFile(enabled ? "1" : "0", kModePath, true);
+    *_aidl_return = enabled;
+    return ndk::ScopedAStatus::ok();
 }
-
+ 
 }  // namespace implementation
-}  // namespace V2_0
 }  // namespace livedisplay
 }  // namespace lineage
 }  // namespace vendor
+} // namespace aidl
---
+++ aidl/livedisplay/SunlightEnhancement.cpp
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+#include <livedisplay/samsung/SunlightEnhancement.h>
+
+#include <fstream>
+
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::base::WriteStringToFile;
+ 
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace implementation {
+
+static constexpr const char* kHbmEnabledPath = "/sys/devices/virtual/panel/brightness/irc_brighter";
+
+ndk::ScopedAStatus SunlightEnhancement::isEnabled(bool* _aidl_return) {
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (!ReadFileToString(kHbmEnabledPath, &tmp)) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+    contents = std::stoi(Trim(tmp));
+
+    *_aidl_return = contents == 1;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus SunlightEnhancement::setEnabled(bool enabled) {
+    if (!WriteStringToFile(enabled ? "1" : "0", kHbmEnabledPath, true)) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace implementation
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
