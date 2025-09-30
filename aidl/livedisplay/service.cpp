27c4c493bf8dbe45a7bedd57eeb55dc670ba94b7
Han Sol Jin <hansol@hansol.ca>
2025-09-30 14:21:15 -0700

    aidl: allow to configure ColorEnhancement
    mh2lm has issues with this setting when UDFPS has been tapped.
    Make this an explicit opt-out.
    
    Change-Id: Ia229ceee90cd98712046ee336f81045f7e23d121

--- aidl/livedisplay/service.cpp
+++ aidl/livedisplay/service.cpp
@@ -28,9 +28,10 @@ int main() {
     std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
 
     // AIDL frontend
-    std::shared_ptr<ColorEnhancement> ce = ndk::SharedRefBase::make<ColorEnhancement>();
     std::shared_ptr<DisplayModes> dm = ndk::SharedRefBase::make<DisplayModes>();
     std::shared_ptr<PictureAdjustment> pa = ndk::SharedRefBase::make<PictureAdjustment>(controller);
+    std::shared_ptr<ColorEnhancement> ce =
+            ENABLE_CE ? ndk::SharedRefBase::make<ColorEnhancement>(): nullptr;
     std::shared_ptr<SunlightEnhancement> se = 
             ENABLE_SE ? ndk::SharedRefBase::make<SunlightEnhancement>() : nullptr;
     binder_status_t status;
