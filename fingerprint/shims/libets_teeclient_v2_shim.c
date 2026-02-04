d87bd7bfc10bb5ae124a76937c05b7bcd7a67f23
pnguyen879 <howardson2001@gmail.com>
2026-02-04 15:21:24 -0600

    fingerprint: Add a shim for libets_teeclient_v2
    libets_teeclient_v2 still depends on libfpsph, so a shim is introduced to stub out the LGE-specific functions in libets_teeclient_v2.
    
    Change-Id: I88fc9fd9d0b3acfa689c7b3f4618f5b04ef6c267

---
+++ fingerprint/shims/libets_teeclient_v2_shim.c
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+void fingerprint_powerhint_start(void) {}
+void fingerprint_powerhint_end(void) {}
